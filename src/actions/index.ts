60b71ad983ca9416803da422f449fa82567e929b
Jakub Biesiada <biesiadajakub@icloud.com>
2020-04-08 19:12:39 +0200

    Update actions

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,5 +1,2 @@
-export const SEARCH = 'SEARCH';
-export const UPDATE_COORDS = 'UPDATE_COORDS';
-export const SYNC_TYPE = 'SYNC_TYPE';
-
-export const SET_THEME = 'SET_THEME';
+export * from './mapActions';
+export * from './themeActions';
---
+++ src/actions/mapActions.ts
@@ -0,0 +1,3 @@
+export const SEARCH = 'SEARCH';
+export const UPDATE_COORDS = 'UPDATE_COORDS';
+export const SYNC_TYPE = 'SYNC_TYPE';
