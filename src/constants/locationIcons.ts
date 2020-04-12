dcd0b53583d626027f3dbe6b947ee0cde5d59dc0
Jakub Biesiada <biesiadajakub@icloud.com>
2020-04-12 12:37:55 +0200

    Add locationIcons

---
+++ src/constants/locationIcons.ts
@@ -0,0 +1,10 @@
+export const locationIcons: any = {
+  place: 'map-marker',
+  boundary: 'path',
+  railway: 'train',
+  amenity: 'drive-time',
+  historic: '',
+  tourism: '',
+  highway: '',
+  leisure: '',
+};
