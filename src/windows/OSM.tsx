b0972df0db57502a9acbe32d4224e2431580ea7f
Jakub Biesiada <biesiadajakub@icloud.com>
2020-04-17 16:45:55 +0200

    Add OSM centering

--- src/windows/OSM.tsx
+++ src/windows/OSM.tsx
@@ -2,16 +2,30 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import { Map, TileLayer } from 'react-leaflet';
 
+import { useGeolocation } from '../hooks/useGeolocation';
+
 import { RootState } from '../reducers';
 
 const OpenStreetMap: React.FC = () => {
   const { coords, zoomLevel } = useSelector((state: RootState) => state.maps);
 
+  const { setCoords, setZoomLevel } = useGeolocation();
+
   return (
-    <Map center={coords} zoom={zoomLevel} animate={false}>
+    <Map
+      center={coords}
+      zoom={zoomLevel}
+      animate={false}
+      onViewportChange={(e) => {
+        setCoords(e.center!);
+
+        setZoomLevel(e.zoom!);
+      }}
+    >
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        on
       />
     </Map>
   );
