159523263139a6212ea5ba837f30420f04656852
Jakub Biesiada <biesiadajakub@icloud.com>
2020-04-30 21:35:13 +0200

    feat: add rehydrate to local storage

--- src/store.ts
+++ src/store.ts
@@ -2,18 +2,20 @@ import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { persistStore, persistReducer } from 'redux-persist';
+import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
 import storage from 'redux-persist/lib/storage';
 
-import rootReducer from 'reducers';
+import rootReducer, { RootState } from 'reducers';
 
 const persistConfig = {
   key: 'store',
   storage,
   blacklist: ['searchResults'],
+  stateReconciler: autoMergeLevel2,
 };
 
 const store = createStore(
-  persistReducer(persistConfig, rootReducer),
+  persistReducer<RootState>(persistConfig, rootReducer),
   composeWithDevTools(applyMiddleware(thunk))
 );
 
