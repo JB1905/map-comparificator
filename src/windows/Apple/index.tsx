c3c53d1127ef3ddeded11fb50c36e89bd6dc4aa7
Jakub Biesiada <biesiadajakub@icloud.com>
2020-04-30 15:21:12 +0200

    refactor: clean up

--- src/windows/Apple/index.tsx
+++ src/windows/Apple/index.tsx
@@ -4,21 +4,12 @@ import { Map } from 'react-mapkit';
 import { useMaps } from 'hooks/useMaps';
 
 const AppleMaps: React.FC = () => {
-  const {
-    coords,
-    // zoomLevel
-  } = useMaps();
+  const { coords } = useMaps();
 
   return (
     <Map
       tokenOrCallback={process.env.REACT_APP_APPLE_MAPS_TOKEN}
       center={coords}
-      // region={{
-      //   latitude: 50,
-      //   longitude: 20,
-      //   latitudeSpan: 0.5,
-      //   longitudeSpan: 0.5,
-      // }}
     />
   );
 };
--- src/windows/Here/index.tsx
+++ src/windows/Here/index.tsx
@@ -1,20 +1,6 @@
 import React from 'react';
-// import HEREMap from '@impargo/react-here-maps';
-
-// import { useMaps } from 'hooks/useMaps';
 
 const HereMaps: React.FC = () => {
-  // const { coords, zoomLevel } = useMaps();
-
-  // return (
-  //   <HEREMap
-  //     appId={process.env.REACT_APP_HERE_MAPS_APP_ID}
-  //     appCode={process.env.REACT_APP_HERE_MAPS_APP_CODE}
-  //     center={{ lat: coords[0], lng: coords[1] }}
-  //     zoom={zoomLevel}
-  //   />
-  // );
-
   return null;
 };
 
