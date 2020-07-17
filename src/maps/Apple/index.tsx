5b89480657802a4d50b5187d3d4611cca4f6db84
Jakub Biesiada <biesiadajakub@icloud.com>
2020-07-17 14:24:29 +0200

    Update index.tsx

--- src/maps/Apple/index.tsx
+++ src/maps/Apple/index.tsx
@@ -8,7 +8,6 @@ const AppleMaps: React.FC = () => {
 
   return (
     <Map
-      data-testid="apple-maps"
       tokenOrCallback={process.env.REACT_APP_APPLE_MAPS_TOKEN}
       center={coords}
     />
