ea995044c674ad92608668e9ac783896c878b0f7
Jakub Biesiada <biesiadajakub@icloud.com>
2020-12-30 20:03:27 +0100

    feat: add error boundary

--- src/collections/maps.tsx
+++ src/collections/maps.tsx
@@ -1,14 +1,33 @@
 import { lazy } from 'react';
 import * as React from 'react';
+import { withErrorBoundary } from 'react-error-boundary';
 
 import LazyComponent from 'components/LazyComponent';
 
-const AppleMaps = lazy(() => import('components/Maps/Apple'));
-const BingMaps = lazy(() => import('components/Maps/Bing'));
-const GoogleMaps = lazy(() => import('components/Maps/Google'));
-const HereMaps = lazy(() => import('components/Maps/Here'));
-const Mapbox = lazy(() => import('components/Maps/Mapbox'));
-const OpenStreetMap = lazy(() => import('components/Maps/OSM'));
+const AppleMaps = withErrorBoundary(
+  lazy(() => import('components/Maps/Apple')),
+  { fallback: null }
+);
+const BingMaps = withErrorBoundary(
+  lazy(() => import('components/Maps/Bing')),
+  { fallback: null }
+);
+const GoogleMaps = withErrorBoundary(
+  lazy(() => import('components/Maps/Google')),
+  { fallback: null }
+);
+const HereMaps = withErrorBoundary(
+  lazy(() => import('components/Maps/Here')),
+  { fallback: null }
+);
+const Mapbox = withErrorBoundary(
+  lazy(() => import('components/Maps/Mapbox')),
+  { fallback: null }
+);
+const OpenStreetMap = withErrorBoundary(
+  lazy(() => import('components/Maps/OSM')),
+  { fallback: null }
+);
 
 export const MAPS: Record<string, React.ReactNode> = {
   'Apple Maps': <LazyComponent component={<AppleMaps />} />,
