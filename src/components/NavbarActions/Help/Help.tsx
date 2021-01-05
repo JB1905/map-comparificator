4107c4ca950ce167c2b928b06b84906509a2425b
Jakub Biesiada <biesiadajakub@icloud.com>
2021-01-05 22:46:57 +0100

    feat: add shortcut for help menu

--- src/components/NavbarActions/Help/Help.tsx
+++ src/components/NavbarActions/Help/Help.tsx
@@ -1,15 +1,19 @@
 import { Button } from '@blueprintjs/core';
+import { useHotkeys } from 'react-hotkeys-hook';
 
 import { useModal } from 'hooks/useModal';
 
 import { ModalType } from 'enums/ModalType';
+import { KeyboardShortcut } from 'enums/KeyboardShortcut';
 
 const Help = () => {
   const { openModal } = useModal();
 
-  return (
-    <Button icon="help" onClick={() => openModal(ModalType.Help)} minimal />
-  );
+  const openHelp = () => openModal(ModalType.Help);
+
+  useHotkeys(KeyboardShortcut.OpenHelp, openHelp);
+
+  return <Button icon="help" onClick={openHelp} minimal />;
 };
 
 export default Help;
