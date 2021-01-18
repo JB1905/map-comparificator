f8e38eb1c5bfa54084503ca6ef2cb7474a329d19
Jakub Biesiada <biesiadajakub@icloud.com>
2021-01-18 22:15:19 +0100

    refactor: update

--- src/hooks/useSearchHistory/index.ts
+++ src/hooks/useSearchHistory/index.ts
@@ -8,11 +8,13 @@ import { LocationIqResult } from 'interfaces/LocationIq';
 
 import type { SearchHistoryItem } from 'types/SearchHistoryItem';
 
+const HISTORY_SIZE_LIMIT = 10;
+
 export const useSearchHistory = () => {
   const dispatch = useDispatch();
 
   const history = useTypedSelector((state) =>
-    state.searchHistory.items.slice(0, 10)
+    state.searchHistory.items.slice(0, HISTORY_SIZE_LIMIT)
   );
 
   const addToHistory = (item: SearchHistoryItem) => {
