623818ea0dfb12718c5c11321058329472f45b19
Jakub Biesiada <biesiadajakub@icloud.com>
2021-01-21 19:51:17 +0100

    test: add search test

---
+++ src/components/SearchForm/SearchForm.test.tsx
@@ -0,0 +1,54 @@
+import { fireEvent, render } from '@testing-library/react';
+
+import SearchForm from './SearchForm';
+
+jest.mock('hooks/useSearchResults', () => ({
+  useSearchResults: () => ({
+    results: [],
+    isLoading: false,
+    error: undefined,
+    query: '',
+    setQuery: jest.fn(),
+  }),
+}));
+
+jest.mock('hooks/useSearchHistory', () => ({
+  useSearchHistory: () => ({
+    history: [],
+    addToHistory: jest.fn(),
+    removeFromHistory: jest.fn(),
+  }),
+}));
+
+jest.mock('hooks/useLayout', () => ({
+  useLayout: () => ({
+    isEmptyLayout: false,
+  }),
+}));
+
+jest.mock('hooks/useMaps', () => ({
+  useMaps: () => ({
+    setCoords: jest.fn(),
+  }),
+}));
+
+jest.mock('react-i18next', () => ({
+  useTranslation: () => ({
+    t: jest.fn(),
+  }),
+}));
+
+describe('SearchForm', () => {
+  it('should', () => {
+    const { container } = render(<SearchForm />);
+
+    const input = container.querySelector(
+      '[type="search"]'
+    ) as HTMLInputElement;
+
+    expect(input).toBeInTheDocument();
+
+    expect(input.value).toBe('');
+    fireEvent.change(input, { target: { value: 'Paris' } });
+  });
+});
