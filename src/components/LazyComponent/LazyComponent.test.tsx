d881d862bc530c6e567ed0f49f32615c36f4ed36
Jakub Biesiada <biesiadajakub@icloud.com>
2021-01-21 18:42:01 +0100

    test: add mock

--- src/components/LazyComponent/LazyComponent.test.tsx
+++ src/components/LazyComponent/LazyComponent.test.tsx
@@ -1,24 +1,24 @@
 import { lazy } from 'react';
 import { render, waitFor } from '@testing-library/react';
 
+jest.mock('../Loader', () => () => <p>Loading...</p>);
+
 import LazyComponent from '.';
 
 const Lazy = lazy(() => import('./LazyComponent.mock'));
 
 describe('LazyComponent', () => {
   it('should render fallback loader', async () => {
-    const { container } = render(<LazyComponent component={<Lazy />} />);
+    const { getByText } = render(<LazyComponent component={<Lazy />} />);
 
-    expect(container.querySelector('.loader')).toBeInTheDocument();
+    expect(getByText('Loading...')).toBeInTheDocument();
   });
 
   it('should render lazy component and hide fallback loader', async () => {
-    const { queryByText, container } = render(
-      <LazyComponent component={<Lazy />} />
-    );
+    const { queryByText } = render(<LazyComponent component={<Lazy />} />);
 
     await waitFor(() => expect(queryByText(`I'm Lazy`)).toBeInTheDocument());
 
-    expect(container.querySelector('.loader')).not.toBeInTheDocument();
+    expect(queryByText('Loading...')).not.toBeInTheDocument();
   });
 });
