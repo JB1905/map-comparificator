a787572a7686f0531c97222043e6a08aa9cb8864
Jakub Biesiada <kbba@op.pl>
2021-01-21 12:56:07 -0500

    test: lazy component (#74)

---
+++ src/components/LazyComponent/LazyComponent.test.tsx
@@ -0,0 +1,27 @@
+import { lazy } from 'react';
+import { render, waitFor } from '@testing-library/react';
+
+
+import LazyComponent from '.';
+
+const DummyLazy = lazy(() => import('./__mocks__/DummyLazy'));
+
+jest.mock('../Loader', () => () => <p>Loading...</p>);
+
+describe('LazyComponent', () => {
+  const componentRenderer = () => render(<LazyComponent component={<DummyLazy />} />)
+
+  it('should render fallback loader', async () => {
+    const { getByText } = componentRenderer();
+
+    expect(getByText('Loading...')).toBeInTheDocument();
+  });
+
+  it('should render lazy component and hide fallback loader', async () => {
+    const { queryByText } = componentRenderer();
+
+    await waitFor(() => expect(queryByText('Dummy Lazy')).toBeInTheDocument());
+
+    expect(queryByText('Loading...')).not.toBeInTheDocument();
+  });
+});
