5818a1f41e44158fc771d9cd04b0048799021ebb
Jakub Biesiada <biesiadajakub@icloud.com>
2021-01-21 20:02:37 +0100

    test: add Help test

---
+++ src/components/NavbarActions/Help/Help.test.tsx
@@ -0,0 +1,19 @@
+import { render } from '@testing-library/react';
+
+import Help from './Help';
+
+jest.mock('react-hotkeys-hook', () => ({
+  useHotkeys: jest.fn(),
+}));
+
+jest.mock('hooks/useModal', () => ({
+  useModal: () => ({
+    openModal: jest.fn(),
+  }),
+}));
+
+describe('Help', () => {
+  it('should render component', () => {
+    const {} = render(<Help />);
+  });
+});
