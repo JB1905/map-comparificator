9fb80f2c0a3efdd06de5135629b390e7e89d5f60
Jakub Biesiada <biesiadajakub@icloud.com>
2021-01-23 15:37:43 +0100

    test: add App.test

---
+++ src/App.test.tsx
@@ -0,0 +1,9 @@
+import { render } from '@testing-library/react';
+
+import App from './App';
+
+describe('App', () => {
+  it.skip('should', async () => {
+    // const { getByText } = render(<App />);
+  });
+});
