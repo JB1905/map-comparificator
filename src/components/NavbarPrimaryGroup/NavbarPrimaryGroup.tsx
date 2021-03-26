d7beacd1ace3c3f9b913aaac025d919989f3fe46
Jakub Biesiada <biesiadajakub@icloud.com>
2021-03-26 16:43:13 +0100

    chore: update

--- src/components/NavbarPrimaryGroup/NavbarPrimaryGroup.tsx
+++ src/components/NavbarPrimaryGroup/NavbarPrimaryGroup.tsx
@@ -9,7 +9,6 @@ import LockToggle from 'components/NavbarActions/LockToggle';
 
 import './NavbarPrimaryGroup.scss';
 
-// TODO
 const NavbarPrimaryGroup = () => (
   <Navbar.Group align={Alignment.LEFT}>
     <Navbar.Heading>
